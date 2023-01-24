a7b4d168700846221b966a0617235341c82ad220
Erick Brandao <brandaoerick@MacBook-Air.local>
2023-01-23 22:53:52 -0300

    ✅ feat: finished Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import "./App.css";
+import { ExerciseDetail,Home } from "./pages";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>title</h1>
-    </div>
+    <Box width="400px">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/exercise/:id" element={<ExerciseDetail/>}/>
+      </Routes>
+      <Footer/>
+    </Box>
   );
 };
 
